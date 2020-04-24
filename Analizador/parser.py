3009674103be55aa81951797d80a1312ab601dc5
Cristina De Leon <cristina.dlmtz@gmail.com>
2020-04-24 01:22:07 -0500

    Se escribieron todas las reglas gramáticas y agregaron TODO comments

--- Analizador/parser.py
+++ Analizador/parser.py
@@ -7,12 +7,12 @@ def p_programa(t):
 
 # VARIABLES
 def p_variables(t):
-    '''variables : VAR tipo : lista_ids SEMICOLON variables_1
+    '''variables : VAR tipo COLON lista_ids SEMICOLON variables_1
                 | empty'''
 def p_variables_1(t):
-    '''variables_1 : tipo : lista_ids SEMICOLON variables_1
+    '''variables_1 : tipo COLON lista_ids SEMICOLON variables_1
                     | empty'''
-                    
+
 # LISTA_IDS
 def p_lista_ids(t):
     '''lista_ids : identificadores lista'''
@@ -22,16 +22,47 @@ def p_lista(t):
             | empty'''
 
 # FUNCIONES
+def p_funciones(t):
+    '''funciones : FUNCION tipo_retorno ID LPAREN parametros RPAREN SEMICOLON variables LCORCHETE estatuto RCORCHETE funciones
+                | empty'''
 
 # TIPO_RETORNO
+def p_tipo_retorno(t):
+    '''tipo_retorno : INT 
+                    | FLOAT 
+                    | CHAR 
+                    | VOID'''
 
 # TIPO
+def p_tipo(t):
+    '''tipo : INT 
+            | FLOAT 
+            | CHAR'''
 
 #PARAMETROS
+def p_parametros(t):
+    '''parametros : tipo COLON ID variables_2
+                    | empty'''
+def p_variables_2(t):
+    '''variables_2 : COMMA tipo COLON variables_2
+                    | empty'''
 
 #ESTATUTO
+def p_estatuto(t):
+    '''estatuto : estatuto_1 estatuto 
+                | empty'''
+def p_estatuto_1(t):
+    '''estatuto_1 : asignacion 
+                    | funcion_void 
+                    | retorno 
+                    | lectura
+                    | escritura 
+                    | decision 
+                    | repeticion'''
 
 #ASIGNACION
+def p_asignacion(t):
+    '''asignacion : identificadores ASIGNAR expresiones SEMICOLON'''
 
 # IDENTIFICADORES
 def p_identificadores(t):
@@ -40,31 +71,99 @@ def p_identificadores(t):
                         | ID '''
 
 # TERMINOS
+def p_terminos(t):
+    '''terminos : LPAREN expresiones RPAREN 
+                | identificadores
+                | var_cte 
+                | funcion_retorno'''
 
 
 # ESPECIALES
+def p_especiales(t):
+    '''especiales : terminos
+                    | terminos especiales_1 especiales'''
+def p_especiales_1(t):
+    '''especiales_1 : DETERMINANTE
+                    | TRANSPUESTA
+                    | INVERSA'''
 
 # FACTORES
+def p_factores(t):
+    '''factores : especiales
+                | especiales factores_1 factores'''
+def p_factores_1(t):
+    '''factores_1 : TIMES 
+                  | DIVIDE'''
 
 # ARITMETICOS
+def p_aritmeticos(t):
+    '''aritmeticos : factores
+                    | factores aritmeticos_1 aritmeticos'''
+def p_aritmeticos_1(t):
+    '''aritmeticos_1 : PLUS 
+                     | MINUS'''
 
 # LOGICOS
+def p_logicos(t):
+    '''logicos : aritmeticos
+                    | aritmeticos logicos_1 logicos'''
+def p_logicos_1(t):
+    '''logicos_1 : LESS 
+                    | GREATER
+                    | EQUALS
+                    | NOTEQUAL'''
 
 # EXPRESIONES
+def p_expresiones(t):
+    '''expresiones : logicos
+                    | logicos expresiones_1 expresiones'''
+def p_expresiones_1(t):
+    '''expresiones_1 : AND 
+                     | OR'''
 
 # FUNCION RETORNO
+def p_funcion_retorno(t):
+    '''funcion_retorno : ID LPAREN lista_ids RPAREN
+                        | ID LPAREN RPAREN'''
 
 # FUNCION VOID
+def p_funcion_void(t):
+    '''funcion_void : ID LPAREN lista_ids RPAREN SEMICOLON
+                        | ID LPAREN RPAREN SEMICOLON'''
 
 # RETORNO
+def p_retorno(t):
+    '''retorno : REGRESA LPAREN expresiones RPAREN SEMICOLON'''
 
 # LECTURA
+def p_lectura(t):
+    '''lectura : LEE LPAREN lista_ids RPAREN SEMICOLON'''
 
 # ESCRITURA
+def p_escritura(t):
+    '''escritura : ESCRIBE LPAREN escritura_1 RPAREN SEMICOLON'''
+def p_escritura_1(t):
+    '''escritura_1 : STRING escritura_2
+                    | expresiones escritura_2'''
+def p_escritura_2(t):
+    '''escritura_2 : COMMA escritura_1 
+                    | empty'''
 
 # DECISION
+def p_decision(t):
+    '''decision : SI LPAREN expresiones RPAREN ENTONCES LCORCHETE estatuto RCORCHETE decision_1'''
+def p_decision_1(t):
+    '''decision_1 : SINO LCORCHETE estatuto RCORCHETE
+                    | empty'''
 
 # REPETICION
+def p_repeticion(t):
+    '''repeticion : repeticion_cond 
+                  | repeticion_no_cond'''
+def p_repeticion_cond(t):
+    '''repeticion_cond : MIENTRAS LPAREN expresiones RPAREN HAZ LCORCHETE estatuto SEMICOLON RCORCHETE'''
+def p_repeticion_no_cond(t):
+    '''repeticion_no_cond : DESDE asignacion HASTA expresiones HACER LCORCHETE estatuto RCORCHETE'''
 
 #VAR_CTE
 def p_var_cte(t):
@@ -92,13 +191,15 @@ parser = yacc.yacc()
 
 while True:
     try:
-        s = input('!Jitters > ') # raw_input para py2
-        file = s
+        s = raw_input('!Jitters > ') # solo 'input()' para py3
+        file = str(s)
+        if s == "":
+            sys.exit('bye')
         f = open(file, 'r')
         data = f.read()
         f.close()
-        if parser.parse(data) == "ACEPTADO":
-            print("ACEPTADO")
+        if parser.parse(data) == "COMPILADO":
+            print("Se compilo exitosamente.")
     except EOFError:
         print(EOFError)
     if not s: continue
