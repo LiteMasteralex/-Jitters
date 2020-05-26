b713e28d1693decef30be3ecc273f766a77b58bd
LiteMasteralex <alexlara_97@hotmail.com>
2020-05-25 21:50:32 -0500

    Se agrego la funcionalidad para que la maquina virtual use las direcciones reales que se generan en compilacion

--- Analizador/virtualMachine.py
+++ Analizador/virtualMachine.py
@@ -1,76 +1,123 @@
 Memoria = {
-	5000: 0,
-	5001: 0,
-	7000: 0,
-	12000: 10
+	'Global': {},
+	'Local': {},
+	'Temporal': {},
+	'Constante': {}
 }
-Quads = [['goto', '', '', 2], ['LEE', 5000, '', ''], ['+', 5000, 12000, 13000], ['=', 13000, '', 5001], ['ESCRIBE', 5001, '', '']]
 
+
+def separaMemoria(contexto, tipo, size):
+	inicio = DefMemoria[contexto][tipo]
+	for loc in range(inicio, inicio + int(size)):
+		if(tipo == 'int'):
+			Memoria[contexto][str(loc)] = 0
+		elif(tipo == 'float'):
+			Memoria[contexto][str(loc)] = 0.0
+		elif(tipo == 'char'):
+			Memoria[contexto][str(loc)] = ''
+		elif(tipo == 'bool'):
+			Memoria[contexto][str(loc)] = False
+		else:
+			Memoria[contexto][str(loc)] = 0
+
+def obtenContexto(loc):
+	loc = int(loc)
+	if(loc < 4000):
+		return 'Global'
+	elif(loc < 7000):
+		return 'Local'
+	elif(loc < 12000):
+		return 'Temporal'
+	else:
+		return 'Constante'
+
+def initContexto(nombre, contexto):
+	separaMemoria(contexto, 'int', TablaFunciones[nombre]['int'])
+	separaMemoria(contexto, 'float', TablaFunciones[nombre]['float'])
+	separaMemoria(contexto, 'char', TablaFunciones[nombre]['char'])
+	separaMemoria('Temporal', 'int', TablaFunciones[nombre]['tint'])
+	separaMemoria('Temporal', 'float', TablaFunciones[nombre]['tfloat'])
+	separaMemoria('Temporal', 'char', TablaFunciones[nombre]['tchar'])
+	separaMemoria('Temporal', 'bool', TablaFunciones[nombre]['bool'])
+	separaMemoria('Temporal', 'addr', TablaFunciones[nombre]['addr'])
 
 class ExecuteError(Exception): pass
 
 def suma(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] + Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] + Memoria[rightCont][right_op]
 	return None
 
 def resta(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] - Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] - Memoria[rightCont][right_op]
 	return None
 
 def multi(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] * Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] * Memoria[rightCont][right_op]
 	return None
 
 def divi(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] / Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] / Memoria[rightCont][right_op]
 	return None
 
 def mayor(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] > Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] > Memoria[rightCont][right_op]
 	return None
 
 def menor(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] < Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] < Memoria[rightCont][right_op]
 	return None
 
 def igual(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] == Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] == Memoria[rightCont][right_op]
 	return None
 
 def noIgual(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] != Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] != Memoria[rightCont][right_op]
 	return None
 
 def And(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] and Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] and Memoria[rightCont][right_op]
 	return None
 
 def Or(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op] or Memoria[right_op]
+	leftCont, rightCont, resCont = obtenContexto(left_op), obtenContexto(right_op), obtenContexto(res)
+	Memoria[resCont][res] = Memoria[leftCont][left_op] or Memoria[rightCont][right_op]
 	return None
 
 def asigna(left_op, right_op, res):
-	Memoria[res] = Memoria[left_op]
+	leftCont, rightCont = obtenContexto(left_op), obtenContexto(right_op)
+	Memoria[leftCont][left_op] = Memoria[rightCont][right_op]
 	return None
+
 
 def escribe(left_op, right_op, res):
 	if(left_op == 'ENDLINE'):
 		print()
-	elif(type(left_op) == str):
-		print(left_op)
+	#elif(type(left_op) == str): TODO: Implementar constantes de tipo letrero que se usan para imprimir
+		#print(left_op)
 	else:
-		print(Memoria[left_op])
+		leftCont = obtenContexto(left_op)
+		print(Memoria[leftCont][left_op])
 	return None
 
 def lee(left_op, right_op, res):
 	lectura = input('!>')
 	value = ''
+	leftCont = obtenContexto(left_op)
 	try:
-		print('Memo:',Memoria)
-		value = type(Memoria[left_op])(lectura)
+		value = type(Memoria[leftCont][left_op])(lectura)
 	except:
 		raise ExecuteError('Error de lectura')
-	Memoria[left_op] = value
+	Memoria[leftCont][left_op] = value
 	return None
 
 def goto(left_op, right_op, res):
@@ -78,7 +125,8 @@ def goto(left_op, right_op, res):
 
 
 def gotoF(left_op, right_op, res):
-	if(left_op):
+	leftCont = obtenContexto(left_op)
+	if(Memoria[leftCont][left_op]):
 		return res
 	return None
 
@@ -102,7 +150,7 @@ def ejecutaQuadruplos():
 			'&': And,
 			'|': Or,
 			'=': asigna,
-			'ESCRIBE': escribe,
+			'ESCRITURA': escribe,
 			'LEE': lee,
 			'Goto': goto,
 			'GotoF': gotoF,
@@ -110,12 +158,33 @@ def ejecutaQuadruplos():
 		}
 		current = Quads[quadNum]
 		fun = switcher.get(current[0], 'err')
-		print(fun)
 		res = fun(current[1], current[2], current[3])
 		if(res != None):
 			quadNum = int(res)
 		else:
 			quadNum = quadNum + 1
+
+
+DefMemoria = {
+    'Global': {
+        'int': 1000,
+        'float': 2000,
+        'char': 3000
+    },
+    'Local': {
+        'int': 4000,
+        'float': 5000,
+        'char': 6000
+    },
+    'Temporal': {
+        'int': 7000,
+        'float': 8000,
+        'char': 9000,
+        'bool': 10000,
+        'addr': 11000
+
+    }
+}
 
 
 import sys
@@ -139,6 +208,7 @@ def main():
 	for line in range(len(fl)):
 		fl[line] = fl[line].rstrip()
 	
+	# Inizalizacion Tabla Funciones
 	while fl[pos] != '>Constantes':
 		name = fl[pos].split()[-1]
 		TablaFunciones[name] = {} 
@@ -150,6 +220,7 @@ def main():
 
 	pos = pos + 1
 
+	# Inizalicacion Constantes
 	while fl[pos] != '>Quads':
 		value = fl[pos]
 		loc = fl[pos + 1].split()[-1]
@@ -164,6 +235,12 @@ def main():
 
 	pos = pos + 1
 
+	# Inizalizacion contexto Global
+	name = fl[1].split()[-1]
+	initContexto(name, 'Global')
+	Memoria['Constante'] = TablaConstantes
+
+	# Inizializacion Quads
 	for qpos in range(pos, len(fl)):
 		quad = (fl[qpos].split())
 		Quads.append(quad)
@@ -173,8 +250,10 @@ def main():
 	print(TablaFunciones)
 	print()
 	print(Quads)
+	print()
+	print(Memoria)
 
-	#ejecutaQuadruplos()
+	ejecutaQuadruplos()
 
 	
 		
